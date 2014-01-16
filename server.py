4156f74772b7eb55240aa28b7d27a46bf55ca331
Allen Koppman <koppmana@gmail.com>
2014-01-16 16:15:37 -0500

    Changes

--- server.py
+++ server.py
@@ -47,13 +47,10 @@ class Cse491server(object):
 def main():
     server = Cse491server()
 
-    print 'Entering infinite loop; hit CTRL-C to exit
+    print 'Entering infinite loop; hit CTRL-C to exit'
     while True:
         server.wait_for_connect()
-        
-    
         server.send_responses()
-        
         server.close_conn()
     
 
