b16ff4dcf1f74e5b1bca13b88da4ddcdc251504e
koppmana <koppmana@gmail.com>
2014-01-21 13:18:56 -0500

    Up to par with hw1 solution and code review changes

--- server.py
+++ server.py
@@ -16,7 +16,8 @@ class Cse491server(object):
         print 'Starting server on', self.host, self.port
         print 'The Web server URL for this would be http://%s:%d/' % (self.host, self.port)
 
-        self.sock.listen(5)                 # Now wait for client connection.
+        self.sock.listen(5)     # Now wait for client connection.
+
 
     def wait_for_connect(self):
         # Establish connection with client.    
@@ -24,19 +25,11 @@ class Cse491server(object):
         
         print 'Got connection from', self.client_host, self.client_port
 
+
     # send response to client
-    def send_responses(self):
-        cont_type = "Content-Type: text/html\n\n"
-        response_type = "HTTP/1.0 "
-        response_line = "200 OK\n"
-        
-        response_body = "<html><body><h1>Hello, world!</h1> \
-                        <p>This is koppmana's web server!</p> \
-                        </body></html>"
-        
-        self.conn.send(response_type + response_line)
-        self.conn.send(cont_type)
-        self.conn.send(response_body)
+    def send_response(self, msg):
+        self.conn.send(msg)
+
                 
     # close socket connection
     def close_conn(self):
@@ -47,10 +40,19 @@ class Cse491server(object):
 def main():
     server = Cse491server()
 
+    cont_type = "Content-Type: text/html\r\n"
+    response_type = "HTTP/1.0"
+    response_line = "200 OK\r\n"
+    response_body = "<html><body><h1>Hello, world!</h1> \
+                    <p>This is koppmana's web server!</p> \
+                    </body></html>"
+
     print 'Entering infinite loop; hit CTRL-C to exit'
     while True:
         server.wait_for_connect()
-        server.send_responses()
+        server.send_response(response_type + " " + response_line)
+        server.send_response(cont_type)
+        server.send_response(response_body)
         server.close_conn()
     
 
